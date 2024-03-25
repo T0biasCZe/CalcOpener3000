cbad18f79460d9fd6b8196b96f11c61f44fc7bdc
T0biasCZe <pekarek.tobias@gmail.com>
2024-03-25 19:36:13 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace CalcOpener3000 {
+	partial class Form1 {
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if(disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			checkBox_opencalc = new CheckBox();
+			timer = new System.Windows.Forms.Timer(components);
+			textBox1 = new TextBox();
+			toolTip1 = new ToolTip(components);
+			checkBox_nomultiplecalcs = new CheckBox();
+			label_aero = new Label();
+			label_numlock = new Label();
+			notifyIcon1 = new NotifyIcon(components);
+			contextMenuStrip1 = new ContextMenuStrip(components);
+			exitToolStripMenuItem = new ToolStripMenuItem();
+			contextMenuStrip1.SuspendLayout();
+			SuspendLayout();
+			// 
+			// checkBox_opencalc
+			// 
+			checkBox_opencalc.AutoSize = true;
+			checkBox_opencalc.BackColor = Color.Transparent;
+			checkBox_opencalc.ForeColor = SystemColors.WindowText;
+			checkBox_opencalc.Location = new Point(12, 30);
+			checkBox_opencalc.Name = "checkBox_opencalc";
+			checkBox_opencalc.Size = new Size(112, 19);
+			checkBox_opencalc.TabIndex = 0;
+			checkBox_opencalc.Text = "Open Calculator";
+			checkBox_opencalc.UseVisualStyleBackColor = false;
+			checkBox_opencalc.CheckedChanged += checkBox_opencalc_CheckedChanged;
+			// 
+			// timer
+			// 
+			timer.Enabled = true;
+			timer.Interval = 500;
+			// 
+			// textBox1
+			// 
+			textBox1.BackColor = Color.White;
+			textBox1.Location = new Point(8, 72);
+			textBox1.Name = "textBox1";
+			textBox1.PlaceholderText = "Calculator path";
+			textBox1.Size = new Size(224, 23);
+			textBox1.TabIndex = 2;
+			textBox1.Text = "C:\\Windows\\System32\\calc.exe";
+			toolTip1.SetToolTip(textBox1, "Enter path to calculator executable that should be opened");
+			// 
+			// checkBox_nomultiplecalcs
+			// 
+			checkBox_nomultiplecalcs.AutoSize = true;
+			checkBox_nomultiplecalcs.BackColor = Color.Transparent;
+			checkBox_nomultiplecalcs.ForeColor = SystemColors.WindowText;
+			checkBox_nomultiplecalcs.Location = new Point(12, 50);
+			checkBox_nomultiplecalcs.Name = "checkBox_nomultiplecalcs";
+			checkBox_nomultiplecalcs.Size = new Size(158, 19);
+			checkBox_nomultiplecalcs.TabIndex = 3;
+			checkBox_nomultiplecalcs.Text = "Dont open multiple calcs";
+			toolTip1.SetToolTip(checkBox_nomultiplecalcs, "If app is already opened, it will focus the already running calculator instead of opening new one");
+			checkBox_nomultiplecalcs.UseVisualStyleBackColor = false;
+			// 
+			// label_aero
+			// 
+			label_aero.AutoSize = true;
+			label_aero.BackColor = Color.Transparent;
+			label_aero.Font = new Font("Segoe UI Semilight", 9F, FontStyle.Italic, GraphicsUnit.Point, 238);
+			label_aero.ForeColor = Color.Transparent;
+			label_aero.Location = new Point(186, 50);
+			label_aero.Name = "label_aero";
+			label_aero.Size = new Size(45, 15);
+			label_aero.TabIndex = 4;
+			label_aero.Text = "aero on";
+			label_aero.Visible = false;
+			// 
+			// label_numlock
+			// 
+			label_numlock.AutoSize = true;
+			label_numlock.BackColor = Color.Transparent;
+			label_numlock.ForeColor = SystemColors.WindowText;
+			label_numlock.Location = new Point(10, 10);
+			label_numlock.Name = "label_numlock";
+			label_numlock.Size = new Size(95, 15);
+			label_numlock.TabIndex = 5;
+			label_numlock.Text = "NumLock is N/A";
+			// 
+			// notifyIcon1
+			// 
+			notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+			notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+			notifyIcon1.Text = "notifyIcon1";
+			notifyIcon1.Visible = true;
+			notifyIcon1.MouseDoubleClick += notifyIcon1_MouseDoubleClick;
+			// 
+			// contextMenuStrip1
+			// 
+			contextMenuStrip1.Items.AddRange(new ToolStripItem[] { exitToolStripMenuItem });
+			contextMenuStrip1.Name = "contextMenuStrip1";
+			contextMenuStrip1.Size = new Size(94, 26);
+			// 
+			// exitToolStripMenuItem
+			// 
+			exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			exitToolStripMenuItem.Size = new Size(93, 22);
+			exitToolStripMenuItem.Text = "Exit";
+			exitToolStripMenuItem.Click += exitToolStripMenuItem_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(238, 106);
+			Controls.Add(label_numlock);
+			Controls.Add(label_aero);
+			Controls.Add(checkBox_nomultiplecalcs);
+			Controls.Add(textBox1);
+			Controls.Add(checkBox_opencalc);
+			ForeColor = Color.Black;
+			MaximizeBox = false;
+			MinimizeBox = false;
+			Name = "Form1";
+			Text = "Calc3000";
+			Deactivate += Form1_Leave;
+			FormClosing += Form1_FormClosing;
+			Leave += Form1_Leave;
+			contextMenuStrip1.ResumeLayout(false);
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private CheckBox checkBox_opencalc;
+		private System.Windows.Forms.Timer timer;
+		private TextBox textBox1;
+		private ToolTip toolTip1;
+		private CheckBox checkBox_nomultiplecalcs;
+		private Label label_aero;
+		private Label label_numlock;
+		private NotifyIcon notifyIcon1;
+		private ContextMenuStrip contextMenuStrip1;
+		private ToolStripMenuItem exitToolStripMenuItem;
+	}
+}
---
+++ Form1.cs
@@ -0,0 +1,220 @@
+using CalcOpener3000.Properties;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using Timer = System.Windows.Forms.Timer;
+
+namespace CalcOpener3000 {
+	public partial class Form1 : Form {
+		public static Form1 form1;
+		public Form1() {
+			InitializeComponent();
+			load_settings();
+			form1 = this;
+			hookId = SetHook(HookCallback);
+
+			timer.Tick += new EventHandler(timer_tick);
+		}
+		void timer_tick(object sender, EventArgs e) {
+			if(Control.IsKeyLocked(Keys.NumLock)) {
+				label_numlock.Text = "NumLock is ON";
+			}
+			else {
+				label_numlock.Text = "NumLock is OFF";
+				SetNumlockOn();
+			}
+		}
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+			UnhookWindowsHookEx(hookId);
+			timer.Stop();
+			save_settings();
+		}
+		private void save_settings() {
+			Settings.Default.calc_path = textBox1.Text;
+			Settings.Default.open_calc = checkBox_opencalc.Checked;
+			Settings.Default.dontopenmultiplecalcs = checkBox_nomultiplecalcs.Checked;
+			Settings.Default.aero = aero;
+			Settings.Default.Save();
+		}
+		private void load_settings() {
+			try {
+				textBox1.Text = Settings.Default.calc_path;
+				checkBox_opencalc.Checked = Settings.Default.open_calc;
+				checkBox_nomultiplecalcs.Checked = Settings.Default.dontopenmultiplecalcs;
+				aero = Settings.Default.aero;
+				label_aero.Visible = aero;
+				if(aero) {
+					this.Text = "";
+					label_numlock.ForeColor = Color.WhiteSmoke;
+					checkBox_nomultiplecalcs.ForeColor = Color.WhiteSmoke;
+					checkBox_opencalc.ForeColor = Color.WhiteSmoke;
+				}
+			}
+			catch(Exception ex) {
+				MessageBox.Show("Settings could not be loaded: " + ex.Message);
+			}
+		}
+		protected override void OnHandleCreated(EventArgs e) {
+			// Use e.g. Color.FromArgb(128, Color.Lime) for a 50% opacity green tint.
+			if(aero)
+				AeroUtil.EnableAcrylic(this, Color.Transparent);
+
+			base.OnHandleCreated(e);
+		}
+		protected override void OnPaintBackground(PaintEventArgs e) {
+			if(aero)
+				e.Graphics.Clear(Color.Transparent);
+			else
+				base.OnPaintBackground(e);
+		}
+		private static void NumlockPressed() {
+			if(Form1.form1.checkBox_opencalc.Checked) {
+				string exe_path = Form1.form1.textBox1.Text;
+				string exe_name = Path.GetFileNameWithoutExtension(exe_path);
+				if(File.Exists(exe_path)) {
+					if(!Form1.form1.checkBox_nomultiplecalcs.Checked || Process.GetProcessesByName(exe_name).Length == 0) {
+						Process.Start(exe_path);
+					}
+					Process[] processes = Process.GetProcessesByName(exe_name);
+
+					while(processes.Length == 0) {
+						processes = Process.GetProcessesByName(exe_name);
+						Thread.Sleep(50);
+					}
+
+					while(processes[0].MainWindowHandle == IntPtr.Zero) {
+						FocusProcess(exe_name);
+						Thread.Sleep(50);
+					}
+					//one more time for good measure
+					Thread.Sleep(100);
+					FocusProcess(exe_name);
+				}
+			}
+		}
+
+		private List<Keys> konamiCode = new List<Keys> { Keys.Up, Keys.Up, Keys.Down, Keys.Down, Keys.Left, Keys.Right, Keys.Left, Keys.Right, Keys.B, Keys.A };
+		private int konamiIndex = 0;
+		bool aero = false;
+		private void konami(Keys KeyCode) {
+			if(KeyCode == konamiCode[konamiIndex]) {
+				konamiIndex++;
+				if(konamiIndex == konamiCode.Count) {
+					konamiIndex = 0;
+					konami_triggered();
+				}
+			}
+			else {
+				konamiIndex = 0; // reset the index if a wrong key is pressed
+			}
+		}
+		private void konami_triggered() {
+			var result = MessageBox.Show("Konami code triggered!\n" +
+				"Do you want to toggle Acrylic? Current status: " + (aero ? "ON" : "OFF") + "\n" +
+				"Changes will be visible after restart", "ACRYLIC", MessageBoxButtons.YesNo);
+			if(result == DialogResult.Yes) {
+				aero = !aero;
+
+			}
+		}
+
+
+		//LOW LEVEL MAGIC, DONT ASK
+
+		private static bool pressed_by_app = false;
+
+		private const int WH_KEYBOARD_LL = 13;
+		private const int WM_KEYDOWN = 0x0100;
+		private static IntPtr hookId = IntPtr.Zero;
+		private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+			using(Process curProcess = Process.GetCurrentProcess())
+			using(ProcessModule curModule = curProcess.MainModule) {
+				return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+			}
+		}
+
+		private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+		private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+			if(nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+				int vkCode = Marshal.ReadInt32(lParam);
+				if(vkCode == (int)Keys.NumLock) {
+					if(!pressed_by_app) NumlockPressed();
+				}
+				else if(Form.ActiveForm == Form1.form1) {
+					Keys key = (Keys)vkCode;
+					form1.konami(key);
+				}
+			}
+			return CallNextHookEx(hookId, nCode, wParam, lParam);
+		}
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+		[DllImport("user32.dll")]
+		private static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+
+		private const int KEYEVENTF_EXTENDEDKEY = 0x0001;
+		private const int KEYEVENTF_KEYUP = 0x0002;
+		private const byte VK_NUMLOCK = 0x90;
+
+		public static void SetNumlockOn() {
+			if(!Control.IsKeyLocked(Keys.NumLock)) {
+				pressed_by_app = true;
+				keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY, UIntPtr.Zero);
+				keybd_event(VK_NUMLOCK, 0, KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP, UIntPtr.Zero);
+				pressed_by_app = false;
+			}
+		}
+
+		private void checkBox_opencalc_CheckedChanged(object sender, EventArgs e) {
+			if(checkBox_opencalc.Checked) {
+				textBox1.Enabled = true;
+			}
+			else {
+				textBox1.Enabled = false;
+			}
+		}
+
+
+		//another magic low level stuff (obviously stolen from stackoverflow :p)
+		[DllImport("user32.dll")]
+		public static extern bool ShowWindowAsync(HandleRef hWnd, int nCmdShow);
+		[DllImport("user32.dll")]
+		public static extern bool SetForegroundWindow(IntPtr WindowHandle);
+		public const int SW_RESTORE = 9;
+
+		private static void FocusProcess(string procName) {
+			Process[] objProcesses = System.Diagnostics.Process.GetProcessesByName(procName);
+			if(objProcesses.Length > 0) {
+				IntPtr hWnd = IntPtr.Zero;
+				hWnd = objProcesses[0].MainWindowHandle;
+				ShowWindowAsync(new HandleRef(null, hWnd), SW_RESTORE);
+				SetForegroundWindow(objProcesses[0].MainWindowHandle);
+			}
+		}
+
+		private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e) {
+			this.Show();
+		}
+
+		private void Form1_Leave(object sender, EventArgs e) {
+			this.Hide();
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
+			Application.Exit();
+		}
+	}
+}
